be01bd6efbccc524fcde3be5404cd2390cca7b1a
javigonzmoya <javigonzmoya@gmail.com>
2020-10-06 17:22:50 +0200

    final seccion4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Err404Component } from './pages/err404/err404.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { AuthRoutingModule } from './auth/auth.routing';
+import { Err404Component } from './err404/err404.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 
 const routes: Routes = [
-  //crear rutas hijas----->
-  { 
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-  
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-
-  { path: '**', component: Err404Component }
+  // path: '/dashboard' PagesRoutingModule
+  // path: '/auth' AuthRoutingModule
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: Err404Component },
 ]
 
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [ RouterModule ]
 })
